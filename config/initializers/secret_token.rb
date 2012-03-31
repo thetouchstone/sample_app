ebfb8f1582fe6bb4a02b76f9a6a40877c83a1745
Joe Clarke <thetouchstonegit@gmail.com>
2012-03-31 13:42:27 +0100

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd197b94f5c571fae46878dccfb888cdae473b36b601214931122d96f7e9926982ceebd107b4f38699965e456853e32b8c47479b9a09931949c310e527d9a02a8'
